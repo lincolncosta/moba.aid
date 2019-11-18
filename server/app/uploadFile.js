226e83dcac07c2f79fd1758de54ea0b403714013
Lincoln <lincoln.mcosta@gmail.com>
2019-11-18 11:05:36 -0300

    Generated experiments files.

--- server/app/uploadFile.js
+++ server/app/uploadFile.js
@@ -2,7 +2,7 @@ const azure = require("azure-storage");
 
 const AZURE_CONTAINER_NAME = "images";
 const AZURE_STORAGE_ACCOUNT_NAME = "mobatorstorage";
-const AZURE_STORAGE_ACCOUNT_ACCESS_KEY = "NmAP6CEEMASkwqfpLybP5jBRqVaBFo6834VXC66D4bFUzfr42mUZnRwwY35xl39yFl86U/blUrBnmaEXMoc2tA==";
+const AZURE_STORAGE_ACCOUNT_ACCESS_KEY = "";
 
 const fileservice = azure.createBlobService(
   AZURE_STORAGE_ACCOUNT_NAME,
