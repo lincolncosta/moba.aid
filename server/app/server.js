e372f82b4ac6e447df0304f3f1db5a07c0d49947
Patrick Santos <patrickspteixeira@gmail.com>
2020-02-22 22:37:44 -0300

    add mongodb connection

--- server/app/server.js
+++ server/app/server.js
@@ -1,7 +1,15 @@
 require('dotenv/config')
 const express = require('express')
 const cors = require('cors')
-const routes = require('./routes')
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('deu boa!')
+});
 
 const server = express();
 
